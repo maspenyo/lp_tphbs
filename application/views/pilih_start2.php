44e46562a109992b4732a5fec3dadd5b5f31497c
maspenyo <eko.gnu@gmail.com>
2021-03-21 18:38:28 +0700

    Update pilih_start2.php

--- application/views/pilih_start2.php
+++ application/views/pilih_start2.php
@@ -3,14 +3,8 @@
     <h3 class="card-title"><?php echo $title; ?></h3>
   </div>
   <div class="card-body">
-  <?php
-    foreach ($hasil as $value) {
-  ?>
     <a href="https://susulan.jogjacbt.web.id/admincbtweb" type="button" class="btn btn-outline-primary btn-block">Proktor</a>
     <a href="https://susulan.jogjacbt.web.id/login" type="button" class="btn btn-outline-success btn-block">Peserta Ujian</a>
-  <?php 
-    }
-  ?>
   </div>
   <div class="card-footer">
     <a href="<?php echo base_url('index.php/main') ?>" class="btn btn-outline-secondary btn-xs">Kembali</a>
